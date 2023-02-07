5c7d3b98848810c4e4d35539bae17fa4081f2b41
David Kelly <dkellino@gmail.com>
2023-02-07 07:05:49 +0000

    updates

---
+++ src/Bayesian/pym.jl
@@ -0,0 +1,146 @@
+using SpecialFunctions;
+using LinearAlgebra;
+using Optim;
+
+@enum PriorType begin
+    default
+    identity
+end
+
+ψ₂(x) = polygamma(2, x)
+
+mm = [1, 2, 3]
+icts = [2, 2, 3]
+
+struct Prior
+    type::PriorType
+    param::Float64
+    prior::Float64
+    d::Float64
+    dprior
+    ddprior::Array{Float64,2}
+end
+
+function convert_from_py_params(α::Float64, δ::Float64)
+    digamma_1_minus_delta = digamma(1.0 - δ)
+    eta = digamma(α + 1.0) - digamma_1_minus_delta
+    gam = (digamma(1.0) - digamma_1_minus_delta) / eta
+
+    return (eta, gam)
+
+end
+
+function common_prior_jacobian(α::Float64, δ::Float64)
+    (eta, gam) = convert_from_py_params(α, δ)
+
+    dha = trigamma(α + 1.0)
+    dhd = trigamma(1.0 - δ)
+    dga = -(gam / eta) * dha
+    dgd = dhd * (digamma(α + 1.0) - digamma(1.0)) / (eta^2.0)
+    J = dha * dgd - dhd * dga
+    return (J, gam, eta, dga, dgd, dha, dhd)
+end
+
+function computePrior(α::Float64, δ::Float64, param::Float64, type::PriorType)
+    # only handling default case at the moment
+    (_, gg_t, hh_t, dga, dgd, dha, dhd) = common_prior_jacobian(α, δ)
+
+    if type == default
+        f = exp(-1 ./ (1 - gg_t) / param)
+        dg = -f / (1 - gg_t)^2 / param
+
+        dp::Vector{Float64} = []
+        push!(dp, dg * dga)
+        push!(dp, dg * dgd)
+
+        ddp = zeros(2, 2)
+
+        ddg = f / (1 - gg_t)^4 / param^2 * (1 + 2 * (gg_t - 1) * param)
+        ddp[1, 1] = ddg * dga^2 + dg * (2 * gg_t / hh_t^2 * dha^2 - gg_t / hh_t * ψ₂(1 + α))
+        ddp[1, 2] = ddg * dga * dgd - dg * (dhd * dha / hh_t^2 - (2 * gg_t * dha * dhd / hh_t^2))
+        ddp[2, 1] = ddp[1, 2]
+        ddp[2, 2] = ddg * dgd^2 + dg * (ψ₂(1.0 - δ) * (digamma(1) - digamma(α + 1.0)) / (hh_t^2) +
+                                        2 * trigamma(1.0 - δ)^2 * (digamma(1) - digamma(1.0 + α)) / hh_t^3)
+
+        return Prior(type, param, f, dg, dp, ddp)
+    else
+        println("not yet done")
+    end
+
+end
+
+function pymPrior(α::Float64, δ::Float64; param::Float64=0.1, type::PriorType=default)::Prior
+    return computePrior(α, δ, param, type)
+end
+
+function logliPyOccupancy(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64})
+    N = dot(mm, icts)
+    K = sum(mm)
+    logp = -gammalndiff(α + 1.0, N - 1.0) +
+           sum(map(x -> logx(α + x * δ), 1:K-1)) +
+           dot(mm, map(x -> gammalndiff(1.0 - δ, Float64(x)), icts .- 1))
+
+    KK = 1:K-1
+    dlogp = zeros(1, 2)
+    Z = 1.0 ./ (α .+ KK .* δ)
+    dlogp[:1] = sum(Z) - digamma(α + N) + digamma(α + 1.0)
+    dlogp[:2] = dot(KK, Z) - dot(mm, digamma.(icts .- δ) .- digamma.(1.0 - δ))
+
+    ddlogp = zeros(2, 2)
+    Z2 = Z .^ 2.0
+    ddlogp[1, 1] = trigamma(1.0 + α) - trigamma(α + N) - sum(Z2)
+    diag = -sum(KK .* Z2)
+    ddlogp[1, :2] = diag
+    ddlogp[2, :1] = diag
+    ddlogp[2, :2] = -sum((KK .^ 2.0) .* Z2) + dot(mm, trigamma.(icts .- δ) .- trigamma.(1.0 - δ))
+
+    return (logp, dlogp, ddlogp)
+
+end
+
+function nlogPostPyoccupancy(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64})
+
+    if α < 0.0 || δ < 0.0 || δ > 1.0
+        return (Inf, -[α, δ])
+    end
+
+
+    (logp, dlogp, ddlogp) = logliPyOccupancy(α, δ, mm, icts)
+    prior = pymPrior(α, δ)
+    nlogp = -logp - logx(prior.prior)
+    ndlogp = -dlogp - prior.dprior' ./ prior.prior
+
+    nddlogp = -ddlogp - (prior.prior * prior.ddprior .- prior.dprior' * prior.dprior) ./ prior.prior^2
+
+    return (nlogp, ndlogp, nddlogp)
+end
+
+@doc raw"""
+    pym(mm::Vector{Int64}, icts::Vector{Int64})::Float64
+
+A more or less faithful port of the original [matlab code](https://github.com/pillowlab/PYMentropy)
+to Julia
+
+"""
+function pym(mm::Vector{Int64}, icts::Vector{Int64})
+    Hbls = 0.0
+    Hvar = 0.0
+    if !any(x -> x > 1, icts)
+        return Inf64
+    end
+
+    # min_alpha = 10 * eps
+
+    N = dot(mm, icts)
+    K = sum(mm)
+
+    if K == 1
+        Hbls = NaN64
+        Hvar = Inf64
+    end
+
+
+
+    Hbls, Hvar
+
+end
