d27b1dd060e0a224365efdb8663631ae41eea3b0
IlariaLaTorre <latorre.ilaria@libero.it>
2023-09-29 09:33:37 +0100

    speed up pym: handled alphas with vectorization

--- src/Estimators/Bayesian/pym.jl
+++ src/Estimators/Bayesian/pym.jl
@@ -7,10 +7,14 @@ using Optim;
     identity
 end
 
+const ComplexOrReal{T} = Union{T,Complex{T}}
+global m
+global icts
+
 ψ₂(x) = polygamma(2, x)
 
-mm = [1, 2, 3]
-icts = [2, 2, 3]
+#mm = [1, 2, 3]
+#icts = [2, 2, 3]
 
 struct Prior
     type::PriorType
@@ -28,47 +32,34 @@ function convert_from_py_params(α::Float64, δ::Float64)
     gam = (digamma(1.0) - digamma_1_minus_delta) / eta
 
     return (eta, gam)
-
-end
-
-function convert_from_py_params(α::Vector{Float64}, δ::Vector{Float64})
-    digamma_1_minus_delta = digamma.(1.0 .- δ)
-    eta = digamma.(α .+ 1.0) .- digamma_1_minus_delta
-    gam = (digamma.(1.0) .- digamma_1_minus_delta) ./ eta
-
-    return (eta, gam)
 end
 
 function common_prior_jacobian(α::Float64, δ::Float64)
     (eta, gam) = convert_from_py_params(α, δ)
-
+    iSz = size(α)
     dha = trigamma(α + 1.0)
     dhd = trigamma(1.0 - δ)
     dga = -(gam / eta) * dha
-    dgd = dhd * (digamma(α + 1.0) - digamma(1.0)) / (eta^2.0)
+    dgd = dhd .* (digamma(α + 1.0) .- digamma.(1.0)) / (eta^2.0)
     J = dha * dgd - dhd * dga
-
-    return (J, gam, eta, dga, dgd, dha, dhd)
-end
-
-function common_prior_jacobian(α::Vector{Float64}, δ::Vector{Float64})
-    (eta, gam) = convert_from_py_params(α, δ)
-    iSz = size(α)
-    dha = trigamma.(α .+ 1.0)
-    dhd = trigamma.(1.0 .- δ)
-    dga = .-(gam ./ eta) .* dha
-    dgd = dhd .* (digamma.(α .+ 1.0) .- digamma.(1.0)) ./ (eta .^ 2.0)
-    J = dha .* dgd .- dhd .* dga
 
     return (J, gam, eta, iSz, dga, dgd, dha, dhd)
 end
 
-function computePrior(α::Float64, δ::Float64, param::Float64, type::PriorType)
+function computePrior(α::Float64, δ::Float64, param::Float64, type::PriorType, op::Integer)
     # only handling default case at the moment
-    (_, gg_t, hh_t, dga, dgd, dha, dhd) = common_prior_jacobian(α, δ)
-
+    (_, gg_t, hh_t, iSz, dga, dgd, dha, dhd) = common_prior_jacobian(α, δ)
+    
     if type == default
+
         f = exp(-1 ./ (1 - gg_t) / param)
+    
+        if op == 1
+
+            return f
+        end 
+    
+
         dg = -f / (1 - gg_t)^2 / param
 
         dp::Vector{Float64} = []
@@ -88,65 +79,53 @@ function computePrior(α::Float64, δ::Float64, param::Float64, type::PriorType)
     else
         println("not yet done")
     end
-
 end
 
-function computePrior(α::Vector{Float64}, δ::Vector{Float64}, param::Float64, type::PriorType)
-    # only handling default case at the moment
-    (_, gg_t, hh_t, iSz, dga, dgd, dha, dhd) = common_prior_jacobian(α, δ)
+pymPrior(a::Number, d::Number, p::Number, t::PriorType, op::Integer) = _pymPrior(float(a), float(d), float(p), t, op)
 
-    p = reshape(exp.(-1.0 ./ (1.0 .- gg_t) ./ param), iSz)
+function _pymPrior(α::ComplexOrReal{Float64}, δ::ComplexOrReal{Float64}, param::Float64, type::PriorType, n_opar::Integer)
+    a = real(α)
+    d = real(δ)
+    p = real(param)
+    t = type
+    op = Integer(n_opar)
 
-    return p
+    return computePrior(a, d, p, t, op)
 end
 
-function pymPrior(α::Float64, δ::Float64; param::Float64=0.1, type::PriorType=default)::Prior
-    return computePrior(α, δ, param, type)
-end
+logliPyOccupancy(a::Number, d::Number, op::Integer) = _logliPyOccupancy(float(a), float(d), op)
 
-function pymPrior(α::Vector{Float64}, δ::Vector{Float64}; param::Float64=0.1, type::PriorType=default)
-    return computePrior(α, δ, param, type)
-end
+function _logliPyOccupancy(α::ComplexOrReal{Float64}, δ::ComplexOrReal{Float64}, n_opar::Integer)
+    # via the derivative of the Kölbig digamma formulation
+    a = real(α)
+    d = real(δ)
+    op = Integer(n_opar)
 
-function logliPyOccupancy(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64})
     N = dot(mm, icts)
     K = sum(mm)
-    logp = -gammalndiff(α + 1.0, N - 1.0) +
-           sum(map(x -> logx(α + x * δ), 1:K-1)) +
-           dot(mm, map(x -> gammalndiff(1.0 - δ, Float64(x)), icts .- 1))
 
-    KK = 1:K-1
-    dlogp = zeros(1, 2)
-    Z = 1.0 ./ (α .+ KK .* δ)
-    dlogp[:1] = sum(Z) - digamma(α + N) + digamma(α + 1.0)
-    dlogp[:2] = dot(KK, Z) - dot(mm, digamma.(icts .- δ) .- digamma.(1.0 - δ))
+    logp = -gammalndiff(a + 1.0, N - 1.0) + sum(map(x -> logx(a + x * d), 1:K-1)) + dot(mm, map(x -> gammalndiff(1.0 - d, Float64(x)), icts .- 1))
+    
+    if op == 1
+        return logp
+    else 
+        KK = 1:K - 1
+        dlogp = zeros(1, 2)
+        Z = 1.0 ./ (a .+ KK .* d)
+        dlogp[:1] = sum(Z) - digamma(a + N) + digamma(a + 1.0)
+        dlogp[:2] = dot(KK, Z) - dot(mm, digamma.(icts .- d) .- digamma.(1.0 - d))
 
-    return (logp, dlogp)
-end
-
-function logliPyOccupancy(α::Vector{Float64}, δ::Vector{Float64}, mm::Vector{Int64}, icts::Vector{Int64})
-    N = dot(mm, icts)
-    K = sum(mm)
-    logp = [0.0 for i in 1:length(α)]
-
-    for adx = 1:length(α)
-        alpha = α[adx]
-        d = δ[adx]
-        logp[adx] = -gammalndiff(alpha + 1.0, N - 1.0) +
-                    sum(map(x -> logx(alpha + x * d), 1:K-1)) +
-                    dot(mm, map(x -> gammalndiff(1.0 - d, Float64(x)), icts .- 1))
+        return logp, dlogp
     end
-
-    return (logp)
 end
 
-function hessian(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64})
+function hessian(α::Float64, δ::Float64)
     N = dot(mm, icts)
     K = sum(mm)
     KK = 1:K-1
     Z = 1.0 ./ (α .+ KK .* δ)
 
-    prior = pymPrior(α, δ)
+    prior = pymPrior(α, δ, 0.1, default, 3)
 
     ddlogp = zeros(2, 2)
     Z2 = Z .^ 2.0
@@ -159,14 +138,15 @@ function hessian(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64
     return -ddlogp - (prior.prior * prior.ddprior .- prior.dprior' .* prior.dprior) ./ prior.prior^2
 end
 
-function nlogPostPyoccupancy(α::Float64, δ::Float64, mm::Vector{Int64}, icts::Vector{Int64})
+function nlogPostPyoccupancy(α::Float64, δ::Float64)
 
     if α < 0.0 || δ < 0.0 || δ > 1.0
         return (Inf, -[α, δ])
     end
 
-    (logp, dlogp) = logliPyOccupancy(α, δ, mm, icts)
-    prior = pymPrior(α, δ)
+    (logp, dlogp) = logliPyOccupancy(α, δ, 2)
+
+    prior = pymPrior(α, δ, 0.1, default, 2)
 
     nlogp = -logp - logx(prior.prior)
 
@@ -192,7 +172,7 @@ function meshgrid(x, y)
     return X, Y
 end
 
-function condH(a, d, mm, icts)
+function condH(a, d)
     m = computeHpy(mm, icts, a[:], d[:])
     m = reshape(m, size(a))
 
@@ -256,6 +236,7 @@ function computeHpy(mm, icts, alphas, ds)
     if N == 0
         # if we have no data, return prior mean & variance
         Hpy = Hpi
+
         return
     end
 
@@ -276,78 +257,6 @@ function computeHpy(mm, icts, alphas, ds)
     Hpy = reshape(Hpy, originalSize)
 
     return Hpy
-end
-
-function reduced_varianceEntropy(mm, icts, alphas, K, flag::Int=0)
-    # v = reduced_varianceEntropy(mm, icts, alphas, K, flag)
-    # Analytical computation of variance of entropy under Dirichlet prior
-
-    # Input
-    #   mm  -
-    #   icts  -
-    #   alpha - parameter of Dirichlet distribution (scalar or vector)
-    #   K     - # bins in distribution
-    #   flag  -
-    #           0: return variance (default)
-    #           1: return 2nd moment
-
-    # Output
-    #   v     - variance (or second moment) of entropy
-
-    # TBD: Vectorize multiple alpha (vector alpha). Currenly uses for loop.
-
-    v = zeros(length(alphas), 1)
-    N = dot(mm, icts)
-
-    # modify mm and icts to account for #zeros (finite bin case)
-    nZ = K - sum(mm)
-
-    # make sure everyone's a column vector
-    mm = mm[:]
-    alphas = alphas[:]
-    icts = icts[:]
-
-    dgNKs = polygamma.(0, N .+ alphas .* K .+ 2)
-    tgNKs = polygamma.(1, N .+ alphas .* K .+ 2)
-    dgAlphas = polygamma.(0, icts .* ones(1, length(alphas)) .+ ones(length(icts), 1) .* alphas' .+ 1)
-    tgAlphas = polygamma.(1, icts .* ones(1, length(alphas)) .+ ones(length(icts), 1) .* alphas' .+ 2)
-
-    for idx = 1:length(alphas)
-        alpha = alphas[idx]
-        aK = alpha * K
-        dgNK = dgNKs[idx]
-        dgAlpha = dgAlphas[:, idx]
-        tgNK = tgNKs[idx]
-        tgAlpha = tgAlphas[:, idx]
-
-        m1 = copy(mm)
-        zcross = copy(m1)
-        m2 = copy((mm .== 1))
-        z1 = copy(m2)
-
-        zcross[(1 .⊻ z1.==1)] = mm[(1 .⊻ z1.==1)] .* (mm[(1 .⊻ z1.==1)] .- 1) / 2
-        zcross[z1.==1] .= 0
-
-        c = (N + aK + 1) .* (N + aK)
-        crossTerms = (icts .+ alpha) .* (icts .+ alpha)' .*
-                     ((dgAlpha .- dgNK) * (dgAlpha .- dgNK)' .- tgNK)
-        q = crossTerms
-        crossTerms = (crossTerms .- diagm(diag(crossTerms))) .* (mm * mm')
-        crossTerms = sum(crossTerms[:]) .+ sum(sum(q .* diagm(zcross))) .* 2
-
-        diagTerms = (icts .+ alpha .+ 1) .* (icts .+ alpha) .*
-                    ((dgAlpha .+ 1.0 ./ (icts .+ alpha .+ 1) .- dgNK) .^ 2 .+ tgAlpha .- tgNK)
-        diagTerms = sum(diagTerms .* mm)
-
-        v[idx] = (crossTerms .+ diagTerms) ./ c
-    end
-
-    if (0 == flag)
-        Sdir2 = computeHdir(mm, icts, K, alphas) .^ 2
-        v = v .- Sdir2
-    end
-
-    return v
 end
 
 function computeHdir(mm, icts, K, alphas)
@@ -395,70 +304,6 @@ function computeHdir(mm, icts, K, alphas)
     Hdir = digamma.(A .+ 1) - (1.0 ./ A) .* ((aa .* digamma.(aa .+ 1)) * mm)
 
     return Hdir
-end
-
-function varianceEntropy(alpha, n)
-    # Analytical computation of variance of entropy under Dirichlet prior
-    # Input
-    #   alpha: (1xM) parameter of Dirichlet distribution
-    #   n: (Kx1) number of observation per bin
-
-    # Output
-    #   v: (1xM) variance of entropy
-    #   moment2: (1xM) second moment of entropy
-    #   m: (1xM) mean of entropy
-
-    # Note that this function doesn't call digamma/trigamma of lightspeed but uses psi of MATLAB
-    # For computing part of PY posterior, alpha is -d.
-
-    # See Also: reduced_varianceEntropy
-    #
-
-    n = n[:]
-    alpha = alpha[:]'
-    K = length(n)
-    N = sum(n)
-    aK = alpha * K
-
-    # vec(aK)[1] temporary -> to check for better structure !!!!!!
-    # aK[:] -> to check for better structure !!!!!!
-    # Mean entropy squared
-    nAlpha = n .* ones(1, length(alpha)) + ones(K, 1) * alpha
-    #nAlpha = n * ones(1, length(alpha)) + ones(K, 1) * alpha;
-    m = polygamma.(0, (N .+ aK .+ 1)) - sum((nAlpha) .* digamma.(nAlpha .+ 1)) ./ (N .+ aK)
-    m2 = m .^ 2
-
-    dgNK = polygamma.(0, N .+ aK[:] .+ 2)
-    tgNK = polygamma.(1, N .+ aK[:] .+ 2)
-    dgAlpha = polygamma.(0, nAlpha .+ 1)
-    tgAlpha = polygamma.(1, nAlpha .+ 2)
-
-    # slightly different from Matlab in results !!!!!!
-    c = (N .+ aK .+ 1) .* (N .+ aK)
-    crossTerms = zeros(size(alpha))
-    #crossTerms = sum((nAlpha).^2) .* ... sum((dgAlpha - dgNK).^2) - tgNK;
-    for ka = 1:length(alpha)
-        # CAUTION! below statement produces outer product of a potentially high
-        # dimensional vector n.
-        tempCrossTerms = nAlpha[:, ka] .* nAlpha[:, ka]' .*
-                         ((dgAlpha[:, ka] .- dgNK[:, ka]) .* (dgAlpha[:, ka] .- dgNK[:, ka])' .- tgNK[:, ka])
-        tempCrossTerms = tempCrossTerms - diagm(diag(tempCrossTerms))
-        crossTerms[ka] = sum(tempCrossTerms[:])
-    end
-
-    diagTerms = (nAlpha .+ 1) .* (nAlpha) .*
-                ((dgAlpha .+ 1.0 ./ (nAlpha .+ 1) .- ones(K, 1) * dgNK) .^ 2
-                 .+
-                 tgAlpha .- ones(K, 1) .* tgNK)
-    diagTerms = sum(diagTerms)
-
-    moment2 = (crossTerms .+ diagTerms) ./ c
-    v = moment2 - m2
-
-    v = max(0, v[:][1])
-    moment2 = max(moment2[:][1], 0)
-
-    return (moment2)
 end
 
 function computeHpyPrior(alphas, ds)
@@ -554,8 +399,11 @@ end
 #------------------------------------------------------------#
 
 #function pym(mm::Vector{Int64}, icts::Vector{Int64})::Float64
-function pym(mm::Vector{Int64}, icts::Vector{Int64})
+function pym(_mm::Vector{Int64}, _icts::Vector{Int64})
     Hbls = 0.0
+
+    global mm = _mm
+    global icts = _icts
  
     if !any(x -> x > 1, icts)
         return Inf64
@@ -572,14 +420,14 @@ function pym(mm::Vector{Int64}, icts::Vector{Int64})
     end
 
     mpt = [1.0, 0.01]
-    nlpy(x) = nlogPostPyoccupancy(x[1], x[2], mm, icts)[1]
+    nlpy(x) = nlogPostPyoccupancy(x[1], x[2])[1]
     res = optimize(nlpy, mpt)
 
     #new mpt
     params = Optim.minimizer(res)
     fval = Optim.minimum(res)
 
-    hess = hessian(params[1], params[2], mm, icts)
+    hess = hessian(params[1], params[2])
 
     fval = -fval
 
@@ -615,10 +463,10 @@ function pym(mm::Vector{Int64}, icts::Vector{Int64})
 
     if Nd * Na < 1e4
         (aa, dd) = meshgrid(ax, dx)
-        loglik = logliPyOccupancy(aa[:], dd[:], mm, icts)
+        loglik = logliPyOccupancy.(aa[:], dd[:], 1)
         lik = exp.(loglik .- maximum(loglik))
-        prior = pymPrior(aa[:], dd[:])
-        mc = condH(aa[:], dd[:], mm, icts)
+        prior = pymPrior.(aa[:], dd[:], 0.1, default, 1)
+        mc = condH(aa[:], dd[:])
         A = ((lik .* prior) .* vec(dw * aw'))
         Z = sum(A)
         Hbls = sum(A .* mc)
