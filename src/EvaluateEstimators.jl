e056e4700991e601964f90049983086cb3c85a0c
IlariaLaTorre <latorre.ilaria@libero.it>
2023-12-13 06:34:27 +0000

    moved to notes

--- src/EvaluateEstimators.jl
+++
@@ -1,9 +0,0 @@
-include("DiscreteEntropy.jl")
-using .DiscreteEntropy
-
-input_size = [25, 50, 100, 200, 400]
-n_runs = 1000
-
-for n in input_size
-    mutual_information_estimation(n, n, n_runs)
-end
