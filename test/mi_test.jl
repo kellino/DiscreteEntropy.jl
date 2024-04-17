536ebe067f6cefc2e4c52a54152b40bb1d5de822
David Kelly <dkellino@gmail.com>
2024-04-17 11:14:45 +0100

    more tests for conditional entropy

--- test/mi_test.jl
+++ test/mi_test.jl
@@ -10,3 +10,10 @@ y = DiscreteEntropy.marginal_counts(m, 2)
 
 @test mutual_information(m, MaximumLikelihood) == estimate_h(from_data(y, Histogram), MaximumLikelihood) - conditional_entropy(m, MaximumLikelihood)
 @test mutual_information(m, ChaoWangJost) == estimate_h(from_data(y, Histogram), ChaoWangJost) - conditional_entropy(m, ChaoWangJost)
+
+
+@test conditional_entropy(from_data(x, Histogram), from_data(m, Histogram), MaximumLikelihood) - conditional_entropy(m, MaximumLikelihood) == 0.0
+@test conditional_entropy(m, Bayes, 0.0) - conditional_entropy(m, MaximumLikelihood) == 0.0
+
+# I don't have a good test for conditional_entropy with NSB, so will just go for regression for the moment
+@test conditional_entropy(m, NSB) == 1.4536388136068217
