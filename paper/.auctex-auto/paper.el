1287042aaedce3ff9492019c3305009e3270b46d
= <dkellino@gmail.com>
2024-04-18 11:54:17 +0100

    wip

---
+++ paper/.auctex-auto/paper.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "paper"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "basharin"
+    "bohme:fse:2020"
+    "blackwell2023hyperfuzzing"
+    "MacKay2003"
+    "Rodriguez2021EntropyEst"
+    "grassberger2008entropy"
+    "chaoshen"
+    "nemenman2002entropy"
+    "zhang"
+    "hausser2009entropy"))
+ '(or :bibtex :latex))
+
